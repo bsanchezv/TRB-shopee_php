224ba13d223e9263783aa040710bbe182bbd9af1
iamsanbarb <bsanchezv@uni.pe>
2020-08-21 18:34:22 -0500

    version 4

--- public/Template/_banner-area.php
+++ public/Template/_banner-area.php
@@ -2,13 +2,13 @@
 <section id="banner-area">
     <div class="owl-carousel owl-theme">
         <div class="item">
-            <img src="./assets/banner1.jpg" alt="banner1">
+            <img src=".public/assets/banner1.jpg" alt="banner1">
         </div>
         <div class="item">
-            <img src="./assets/banner2.jpg" alt="banner2">
+            <img src=".public/assets/banner2.jpg" alt="banner2">
         </div>
         <div class="item">
-            <img src="./assets/banner3.jpg" alt="banner3">
+            <img src=".public/assets/banner3.jpg" alt="banner3">
         </div>
     </div>
 </section>
--- public/Template/notFound/_wishlist_notFound.php
+++ public/Template/notFound/_wishlist_notFound.php
@@ -10,7 +10,7 @@
                 <!-- Empty Cart -->
                 <div class="row border-top py-3 mt-3">
                     <div class="col-sm-12 text-center py-2">
-                        <img src="./assets/empty_cart.png" alt="Empty Cart" class="img-fluid" style="height: 200px;">
+                        <img src=".public/assets/empty_cart.png" alt="Empty Cart" class="img-fluid" style="height: 200px;">
                         <p class="font-baloo font-size-16 text-black-50">Lista de deseos vacía</p>
                     </div>
                 </div>
