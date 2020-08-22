ac3180b4523396a55712e4cabc0cf71930b4fa8f
iamsanbarb <bsanchezv@uni.pe>
2020-08-21 19:36:55 -0500

    version 4

--- public/footer.php
+++ public/footer.php
@@ -61,7 +61,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha256-CBrpuqrMhXwcLLUd5tvQ4euBHCdh7wGlDfNz8vbu/iI=" crossorigin="anonymous"></script>
 
 <!--Custom Javascript-->
-<script src="index.js"></script>
+<script src="../index.js"></script>
 
 </body>
 </html>
--- public/header.php
+++ public/header.php
@@ -38,7 +38,7 @@
 
     <!--Primary Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light color-second-bg">
-        <a class="navbar-brand" href="index.php">TRB</a>
+        <a class="navbar-brand" href="../index.php">TRB</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -46,7 +46,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav m-auto font-rubik">
                 <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="../index.php">Inicio <span class="sr-only">(current)</span></a>
                 </li>
                 <li
                 <li class="nav-item active">
