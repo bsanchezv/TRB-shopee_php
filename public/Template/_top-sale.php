b724455beb07f5196772b2024d5c7a73b350524d
iamsanbarb <bsanchezv@uni.pe>
2020-08-25 00:10:33 -0500

    version 8.2

--- public/Template/_top-sale.php
+++ public/Template/_top-sale.php
@@ -22,8 +22,6 @@ if (isset($_POST['top_sale_submit'])){
             <div class="item py-2">
                 <div class="product font-rale">
 
-                    <a href="<?php printf('%s?item_id=%s', 'public/product.php',  $item['item_id']); ?>"><img src="<?php echo $item['imagen']??"public/assets/Productos/1.jpg"; ?>" alt="product1" class="img-fluid"></a>
-
                     <a href="<?php printf('%s?item_id=%s', 'public/product.php',  $item['item_id']); ?>"><img src="./admin<?php echo $item['imagen']??"public/assets/Productos/1.jpg"; ?>" alt="product1" class="img-fluid"></a>
 
                     <div class="text-center">
