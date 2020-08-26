9bfa0bfaed1ba9a92c54bd3fa3fa38b8d27bc7a1
iamsanbarb <bsanchezv@uni.pe>
2020-08-26 00:14:11 -0500

    version 8.5

--- public/header.php
+++ public/header.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
 
     <!--Custom CSS file-->
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href=$_SERVER['DOCUMENT_ROOT']."/style.css">
 
     <?php
     //require functions.php file
