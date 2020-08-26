df14a57b26d3e23ddf2f13b4c64644101716880e
iamsanbarb <bsanchezv@uni.pe>
2020-08-25 21:09:38 -0500

    version 8.5

--- public/wishlist.php
+++ public/wishlist.php
@@ -1,23 +1,23 @@
 <?php
 ob_start();
 //Include header.php file
-include('public/header.php');
+include('header.php');
 ?>
 
 <?php
 
 //Include _wishlist_template file if it is not empty. (if)
-count($product->getData('wishlist')) ? include('public/Template/_wishlist_template.php') : include('public/Template/notFound/_wishlist_notFound.php');
+count($product->getData('wishlist')) ? include('Template/_wishlist_template.php') : include('Template/notFound/_wishlist_notFound.php');
 
 //Include _cart-template if it is not empty. (if)
-count($product->getData('cart')) ? include('public/Template/_cart-template.php') : include('public/Template/notFound/_cart_notFound.php');
+count($product->getData('cart')) ? include('Template/_cart-template.php') : include('Template/notFound/_cart_notFound.php');
 
 //Include _new-products file
-include('public/Template/_new-products.php');
+include('Template/_new-products.php');
 
 ?>
 
 <?php
 //Include footer.php file
-include('public/footer.php');
+include('footer.php');
 ?>
